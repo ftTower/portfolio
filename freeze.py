097f0d0ed3e2d636ca25fb01fa311d5d15ba61e7
ftTower <thomas.auer67@gmail.com>
2025-08-09 19:12:39 +0200

    Add route generation for static pages in freeze.py

--- freeze.py
+++ freeze.py
@@ -3,5 +3,15 @@ from app import app
 
 freezer = Freezer(app)
 
+# This is a list of all your routes that need to be generated as static pages
+@freezer.register_generator
+def generate_pages():
+    yield '/'
+    yield '/a-propos'
+    yield '/projets'
+    yield '/blog'
+    yield '/contact'
+    yield '/cv'
+
 if __name__ == '__main__':
     freezer.freeze()
