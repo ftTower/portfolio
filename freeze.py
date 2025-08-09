88ea9ec348b0e8c1cccd2d1845cd7d9608e164e5
ftTower <thomas.auer67@gmail.com>
2025-08-09 19:21:07 +0200

    Update page generation in freeze.py to yield specific routes for static site generation

--- freeze.py
+++ freeze.py
@@ -1,20 +1,18 @@
 from flask_frozen import Freezer
 from app import app
 
-# This is important for generating relative URLs
-# It creates a structure like /blog/index.html instead of /blog.html
 app.config['FREEZER_DESTINATION_ROOT'] = 'build'
 
 freezer = Freezer(app)
 
 @freezer.register_generator
 def generate_pages():
-    yield 'index'
-    yield 'about'
-    yield 'projects'
-    yield 'blog'
-    yield 'contact'
-    yield 'cv'
+    yield '/'
+    yield '/a-propos'
+    yield '/projets'
+    yield '/blog'
+    yield '/contact'
+    yield '/cv'
 
 if __name__ == '__main__':
     freezer.freeze()
